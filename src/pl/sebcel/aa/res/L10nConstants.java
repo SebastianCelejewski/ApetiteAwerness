26ddaa04dd7097197c29cdb0d91dc086d0a3723a
Sebastian Celejewski <Sebastian.Celejewski@wp.pl>
2017-12-17 07:28:31 +0100

    Initial commit

---
+++ src/pl/sebcel/aa/res/L10nConstants.java
@@ -0,0 +1,20 @@
+package pl.sebcel.aa.res;
+/**
+ *	L10nConstants class is auto generated. DO NOT CHANGE IT.
+ */
+public class L10nConstants {
+	
+    /**
+ 	 *	LOCALE NAMES
+ 	 */
+    public class locales {
+
+    }
+
+    /**
+ 	 *	KEY VALUES
+ 	 */    
+    public class keys {
+
+    }
+}
